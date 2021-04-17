6bf96e3451bc2f835891fb9190060e07c432ecfb
bettech123 <bethel89ogu@gmail.com>
2021-04-17 00:10:36 -0800

    iniial commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("LAB2 \n Given two numbers, return true if the sum of both numbers is less than 100. Otherwise return false.");
+           
+
+            int num1;
+            int num2;
+            bool result1 = true; 
+            bool result2 = false;
+
+            Console.WriteLine("Enter first number ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter second number");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            if(num1 + num2 < 100)
+            {
+                 Console.WriteLine(result1);
+            }
+            else
+            {
+                Console.WriteLine(result2);
+            }
+
+
+        }
+    }
+}
